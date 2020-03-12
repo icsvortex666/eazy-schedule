ac3d5d9d946a031d98bc1360a83f9a1729a55308
Amit Feldman <amit@feldman.cl>
2020-03-12 15:17:13 +0200

    create Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+const Navbar: FC = () => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        EazySchedule
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
