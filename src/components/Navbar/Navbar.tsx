1d370a2993404feec393b16b83f901854aa1d4fa
Amit Feldman <amit@feldman.cl>
2020-03-12 19:36:38 +0200

    change navbar-brand font

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 const Navbar: FC = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark px-0">
+    <nav className="navbar navbar-dark bg-dark p-0">
       <a className="navbar-brand font-weight-bold mx-auto" href="/">
         EazySchedule
       </a>
