288221ae8967ba2efd3a406a46177d07451f9ac2
Amit Feldman <amit@feldman.cl>
2020-03-12 18:18:19 +0200

    update table and include current day highlight

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,14 +1,21 @@
 import React, { FC } from 'react';
+import CN from 'classnames';
 
-import { TableProps } from 'interfaces';
+import { TableProps, EventProp } from 'interfaces';
 
 const Table: FC<TableProps> = ({ table, dates }) => {
+  const currentDay = new Date()
+    .toDateString()
+    .replace(/\d{4}$/, '')
+    .replace(' ', ', ')
+    .trimEnd();
+
   return (
     <div className="table-responsive">
       <table className="table table-bordered">
         <thead className="thead-light text-center">
           <tr>
-            <th scope="col">#</th>
+            <th scope="col" />
             {dates.map((date, idx) => (
               <th key={idx} scope="col" title={date}>
                 {date}
@@ -23,23 +30,26 @@ const Table: FC<TableProps> = ({ table, dates }) => {
               <th scope="row" className="text-center align-middle">
                 {time}
               </th>
-              {events.map(
-                (
-                  {
-                    title,
-                    description
-                  }: {
-                    title: string;
-                    description: string;
-                  },
-                  d: number
-                ) => (
-                  <td key={d} title={`${dates[d]} - ${time}`}>
-                    <p className="mb-0">{title}</p>
-                    <small>{description}</small>
-                  </td>
-                )
-              )}
+              {events.map((event: any, d: number) => (
+                <td
+                  key={d}
+                  title={`${dates[d]} - ${time}`}
+                  className={CN({
+                    'today': currentDay === dates[d]
+                  })}
+                >
+                  <div className="row">
+                    {event.map(
+                      ({ title, description }: EventProp, idx: number) => (
+                        <div key={idx} className="col">
+                          <p className="mb-0">{title}</p>
+                          <small>{description}</small>
+                        </div>
+                      )
+                    )}
+                  </div>
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
