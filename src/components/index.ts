1bf2ca0f8058c76d28a3926aa177c0401c6415bf
Amit Feldman <amit@feldman.cl>
2020-03-12 15:17:42 +0200

    update components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,13 @@
 import App from './App';
+import Main from './Main';
+import Navbar from './Navbar';
+import Table from './Table';
+import Modal from './Modal';
 
 export {
-  App
+  App,
+  Main,
+  Navbar,
+  Table,
+  Modal
 };
