f9f2dafbed66b90e07ce921cc7fd64723ac41eac
Amit Feldman <amit@feldman.cl>
2020-03-12 19:23:48 +0200

    remove padding x axis to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 const Navbar: FC = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-dark bg-dark px-0">
       <a className="navbar-brand font-weight-bold mx-auto" href="/">
         EazySchedule
       </a>
