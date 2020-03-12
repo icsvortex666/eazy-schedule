b6551e4ffc44606c9f42a3ec321819197925fc5d
Amit Feldman <amit@feldman.cl>
2020-03-12 15:18:23 +0200

    update App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,28 @@
 import React, { FC } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import { Home as HomePage } from 'pages';
 
 const App: FC = () => {
   return (
-    <>
-      App rendered
-    </>
+    <Router>
+      <Switch>
+        <Route
+          exact={true}
+          path="/"
+          component={HomePage}
+        />
+        <Route
+          exact={true}
+          path="*"
+          component={HomePage}
+        />
+      </Switch>
+    </Router>
   );
 };
 
