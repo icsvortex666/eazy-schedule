d77e6f1d764fd93d59223699a9cc8f69b05f4fef
Amit Feldman <amit@feldman.cl>
2020-03-13 22:07:46 +0200

    update table component and import event component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
 import CN from 'classnames';
 
-import { TableProps, EventProps } from 'interfaces';
+import {
+  TableProps,
+  TableEventsProps,
+  EventProps
+} from 'interfaces';
 
 import {
   IconHeader,
   TimeHeader,
-  DateHeader
+  DateHeader,
+  Event
 } from 'components';
 
 const Table: FC<TableProps> = ({ table, dates }) => {
@@ -16,44 +21,55 @@ const Table: FC<TableProps> = ({ table, dates }) => {
     .replace(' ', ', ')
     .trimEnd();
 
+  const tableHead = (
+    <thead className="thead-light text-center">
+      <tr>
+        <IconHeader />
+        {dates.map((date: string, DatesIdx: number) => (
+          <DateHeader
+            key={DatesIdx}
+            date={date}
+          />
+        ))}
+      </tr>
+    </thead>
+  );
+
+  const tableBody = (
+    <tbody>
+      {table.map(({ time, events }: TableEventsProps, TimeIdx: number) => (
+        <tr key={TimeIdx}>
+          <TimeHeader time={time} />
+          {events.map((event: any[], DateIdx: number) => (
+            <td
+              key={DateIdx}
+              title={`${dates[DateIdx]} - ${time}`}
+              className={CN({ today: currentDay === dates[DateIdx] })}
+            >
+              <div className="row">
+                {event.map(
+                  ({ title, description }: EventProps, EventIdx: number) => (
+                    <Event
+                      key={EventIdx}
+                      time={time}
+                      title={title}
+                      description={description}
+                    />
+                  )
+                )}
+              </div>
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+
   return (
     <div className="table-responsive">
       <table className="table table-bordered">
-        <thead className="thead-light text-center">
-          <tr>
-            <IconHeader />
-            {dates.map((date, idx) => (
-              <DateHeader key={idx} date={date} />
-            ))}
-          </tr>
-        </thead>
-
-        <tbody>
-          {table.map(({ time, events }, t) => (
-            <tr key={t}>
-              <TimeHeader time={time} />
-              {events.map((event: any, d: number) => (
-                <td
-                  key={d}
-                  title={`${dates[d]} - ${time}`}
-                  className={CN({ today: currentDay === dates[d] })}
-                >
-                  <div className="row">
-                    {event.map(
-                      ({ title, description }: EventProps, idx: number) => (
-                        <div key={idx} className="col event px-1">
-                          <small className="text-secondary">{time}</small>
-                          <p className="mb-0">{title}</p>
-                          <small>{description}</small>
-                        </div>
-                      )
-                    )}
-                  </div>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+        {tableHead}
+        {tableBody}
       </table>
     </div>
   );
