70bda8c75be2055371a302c5ee3583c45048b0bb
Amit Feldman <amit@feldman.cl>
2020-03-14 16:51:58 +0200

    refactor generators

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -1,25 +1,20 @@
+import { dateWithFormat } from 'helpers';
+
 export const hoursGenerator = () => (
   Array.from(Array(24), (_, idx) => (
     idx < 10 ? `0${idx}:00` : `${idx}:00`
   ))
 );
 
-export const datesGenerator = () => {
+export const datesGenerator = (currentDate: Date) => {
   const datesList: Array<string> = [];
-  const currentDate: Date = new Date();
 
   currentDate.setDate(
     currentDate.getDate() - currentDate.getDay()
   );
 
   for (let i = 0; i < 7; i += 1) {
-    const dateWithFormat = new Date(currentDate)
-      .toDateString()
-      .replace(/\d{4}$/, '')
-      .replace(' ', ', ')
-      .trimEnd();
-
-    datesList.push(dateWithFormat);
+    datesList.push(dateWithFormat(currentDate));
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
